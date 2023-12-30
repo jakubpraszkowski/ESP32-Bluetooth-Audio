6628db34a89162a8f26fa28d779f147b3c480238
Jakub Praszkowski <kubapraszkowski@gmail.com>
2023-12-30 20:59:34 +0100

    code refactor. Get rid of obsolete functions

--- main/bt_app_av.h
+++ main/bt_app_av.h
@@ -13,45 +13,192 @@
 #include "esp_bt_main.h"
 #include "esp_bt_device.h"
 #include "esp_gap_bt_api.h"
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+
 #include "driver/dac_continuous.h"
+
 #include "sys/lock.h"
 
 /* log tags */
-#define BT_AV_TAG       "BT_AV"
-#define BT_RC_TG_TAG    "RC_TG"
-#define BT_RC_CT_TAG    "RC_CT"
+#define BT_AV_TAG "BT_AV"
+#define BT_RC_TG_TAG "RC_TG"
+#define BT_RC_CT_TAG "RC_CT"
+
+/* AVRCP used transaction labels */
+#define APP_RC_CT_TL_GET_CAPS (0)
+#define APP_RC_CT_TL_GET_META_DATA (1)
+#define APP_RC_CT_TL_RN_TRACK_CHANGE (2)
+#define APP_RC_CT_TL_RN_PLAYBACK_CHANGE (3)
+#define APP_RC_CT_TL_RN_PLAY_POS_CHANGE (4)
+
+/* Application layer causes delay value */
+#define APP_DELAY_VALUE 50 // 5ms
 
 /**
- * @brief  callback function for A2DP sink
+ * @brief Allocates a buffer for metadata and copies the metadata into it.
  *
- * @param [in] event  event id
- * @param [in] param  callback parameter
+ * This function allocates a buffer of size equal to the length of the metadata plus one,
+ * then copies the metadata into this buffer. The last byte of the buffer is set to 0.
+ * The metadata pointer is then updated to point to this new buffer.
+ *
+ * @param param Pointer to the metadata parameter. This should point to a variable of type esp_avrc_ct_cb_param_t.
+ */
+void bt_app_alloc_meta_buffer(esp_avrc_ct_cb_param_t *param);
+
+/**
+ * @brief Handles a new track event in the Bluetooth Audio Video Remote Control (AVRC) profile.
+ *
+ * This function requests metadata for the new track, including title, artist, album, and genre.
+ * If the peer device supports track change notifications, it also registers for such notifications.
+ */
+void bt_av_new_track(void);
+
+/**
+ * @brief Handles a playback status change event in the Bluetooth Audio Video Remote Control (AVRC) profile.
+ *
+ * This function checks if the peer device supports playback status change notifications.
+ * If it does, the function registers for such notifications.
+ */
+void bt_av_playback_changed(void);
+
+/**
+ * @brief Handles a playback position change event in the Bluetooth Audio Video Remote Control (AVRC) profile.
+ *
+ * This function checks if the peer device supports playback position change notifications.
+ * If it does, the function registers for such notifications.
+ */
+void bt_av_play_pos_changed(void);
+
+/**
+ * @brief Handles notifications for the Bluetooth Audio Video Remote Control (AVRC) profile.
+ *
+ * This function handles various events related to the AVRC profile. When a new track is loaded,
+ * it calls the bt_av_new_track function. When the playback status changes, it logs the new status
+ * and calls the bt_av_playback_changed function. When the playback position changes, it logs the
+ * new position and calls the bt_av_play_pos_changed function. If the event is not one of these,
+ * it logs that the event is unhandled.
+ *
+ * @param event_id The ID of the event.
+ * @param event_parameter Pointer to the parameter of the event. This can be NULL if no parameter is needed.
+ */
+void bt_av_notify_evt_handler(uint8_t event_id, esp_avrc_rn_param_t *event_parameter);
+
+/**
+ * @brief Installs the I2S driver for the Bluetooth application.
+ *
+ * This function configures the DAC for continuous operation with a specific configuration.
+ * It allocates continuous channels with the given configuration and enables them. The configuration includes parameters
+ * such as the channel mask, descriptor number, buffer size, frequency, offset, clock source, and channel mode.
+ */
+void bt_i2s_driver_install(void);
+
+/**
+ * @brief Uninstalls the I2S driver for the Bluetooth application.
+ *
+ * This function disables the continuous operation of the DAC and deletes the continuous channels.
+ */
+void bt_i2s_driver_uninstall(void);
+
+/**
+ * @brief Sets the volume level as specified by a remote controller.
+ *
+ * This function logs the new volume level and sets the volume to the specified level.
+ * The volume level is expected to be a value between 0 and 127 (0x7f), inclusive.
+ * The function acquires a lock before changing the volume and releases it afterwards
+ * to prevent other tasks from changing the volume at the same time.
+ *
+ * @param volume The new volume level, as a value between 0 and 127 (0x7f), inclusive.
+ */
+void volume_set_by_controller(uint8_t volume);
+
+/**
+ * @brief Sets the volume level as specified by the local host.
+ *
+ * This function logs the new volume level and sets the volume to the specified level.
+ * The volume level is expected to be a value between 0 and 127 (0x7f), inclusive.
+ * The function acquires a lock before changing the volume and releases it afterwards
+ * to prevent other tasks from changing the volume at the same time.
+ * If a volume change notification was previously requested by a remote AVRCP controller,
+ * it sends a notification response to the controller with the new volume level.
+ *
+ * @param volume The new volume level, as a value between 0 and 127 (0x7f), inclusive.
+ */
+void volume_set_by_local_host(uint8_t volume);
+
+/**
+ * @brief Handles the A2DP events.
+ *
+ * This function is called when an A2DP event occurs. It handles various events such as connection state changes,
+ * audio stream transmission state changes, audio codec configuration, A2DP initialization and deinitialization,
+ * protocol service capabilities configuration, delay value setting and getting.
+ *
+ * @param event The A2DP event type.
+ * @param p_param The event parameter, which is cast to the appropriate type based on the event type.
+ */
+void bt_av_hdl_a2d_evt(uint16_t event, void *p_param);
+
+/**
+ * @brief Handles the AVRCP Controller events.
+ *
+ * This function is called when an AVRCP Controller event occurs. It handles various events such as connection state changes,
+ * passthrough responses, metadata responses, change notifications, remote features indication, and notification capabilities
+ * of the peer device. For each event, it logs the event details and performs the appropriate action.
+ *
+ * @param event The AVRCP Controller event type.
+ * @param p_param The event parameter, which is cast to the appropriate type based on the event type.
+ */
+void bt_av_hdl_avrc_ct_evt(uint16_t event, void *p_param);
+
+/**
+ * @brief Handles the AVRCP Target events.
+ *
+ * This function is called when an AVRCP Target event occurs. It handles various events such as passthrough commands,
+ * absolute volume setting commands, notification registration, remote features indication, and others.
+ * For each event, it logs the event details and performs the appropriate action.
+ *
+ * @param event The AVRCP Target event type.
+ * @param p_param The event parameter, which is cast to the appropriate type based on the event type.
+ */
+void bt_av_hdl_avrc_tg_evt(uint16_t event, void *p_param);
+
+/**
+ * @brief Callback function for A2DP events.
+ *
+ * This function is called when an A2DP event occurs. It handles various events such as connection state changes,
+ * audio stream transmission state changes, audio codec configuration, A2DP initialization and deinitialization,
+ * protocol service capabilities configuration, delay value setting and getting. For each event, it dispatches the
+ * event to the bt_av_hdl_a2d_evt function for handling. If an invalid event is received, it logs an error message.
+ *
+ * @param event The A2DP event type.
+ * @param param The event parameter, which is cast to the appropriate type based on the event type.
  */
 void bt_app_a2d_cb(esp_a2d_cb_event_t event, esp_a2d_cb_param_t *param);
 
 /**
- * @brief  callback function for A2DP sink audio data stream
+ * @brief Callback function for AVRCP Controller events.
  *
- * @param [out] data  data stream writteen by application task
- * @param [in]  len   length of data stream in byte
- */
-void bt_app_a2d_data_cb(const uint8_t *data, uint32_t len);
-
-/**
- * @brief  callback function for AVRCP controller
+ * This function is called when an AVRCP Controller event occurs. It handles various events such as metadata responses,
+ * connection state changes, passthrough responses, change notifications, remote features indication, and notification
+ * capabilities of the peer device. For each event, it dispatches the event to the bt_av_hdl_avrc_ct_evt function for handling.
+ * If an invalid event is received, it logs an error message.
  *
- * @param [in] event  event id
- * @param [in] param  callback parameter
+ * @param event The AVRCP Controller event type.
+ * @param param The event parameter, which is cast to the appropriate type based on the event type.
  */
 void bt_app_rc_ct_cb(esp_avrc_ct_cb_event_t event, esp_avrc_ct_cb_param_t *param);
 
 /**
- * @brief  callback function for AVRCP target
+ * @brief Callback function for AVRCP Target events.
  *
- * @param [in] event  event id
- * @param [in] param  callback parameter
+ * This function is called when an AVRCP Target event occurs. It handles various events such as connection state changes,
+ * passthrough commands, absolute volume setting commands, notification registration, and player application setting commands.
+ * For each event, it dispatches the event to the bt_av_hdl_avrc_tg_evt function for handling. If an invalid event is received,
+ * it logs an error message.
+ *
+ * @param event The AVRCP Target event type.
+ * @param param The event parameter, which is cast to the appropriate type based on the event type.
  */
 void bt_app_rc_tg_cb(esp_avrc_tg_cb_event_t event, esp_avrc_tg_cb_param_t *param);
 
--- main/bt_app_core.h
+++ main/bt_app_core.h
@@ -14,91 +14,193 @@
 #include "freertos/queue.h"
 #include "freertos/semphr.h"
 #include "freertos/task.h"
-#include "esp_log.h"
-#include "driver/dac_continuous.h"
 #include "freertos/ringbuf.h"
 
-#define RINGBUF_HIGHEST_WATER_LEVEL    (32 * 1024)
-#define RINGBUF_PREFETCH_WATER_LEVEL   (20 * 1024)
+#include "esp_log.h"
 
-/* log tag */
-#define BT_APP_CORE_TAG    "BT_APP_CORE"
+#include "driver/dac_continuous.h"
+
+#define RINGBUF_MAX_BYTES_BUFFER (32 * 1024)
+#define RINGBUF_PREFETCH_START (20 * 1024)
+
+#define BT_APP_CORE_TAG "BT_APP_CORE"
 
 /* signal for `bt_app_work_dispatch` */
-#define BT_APP_SIG_WORK_DISPATCH    (0x01)
-
-typedef enum {
-    PROCESSING,    /* ringbuffer is buffering incoming audio data, I2S is working */
-    PREFETCHING,   /* ringbuffer is buffering incoming audio data, I2S is waiting */
-    DROPPING       /* ringbuffer is not buffering (dropping) incoming audio data, I2S is working */
-} Ringbuffer_mode;
+#define BT_APP_SIG_WORK_DISPATCH (0x01)
 
 /**
- * @brief  handler for the dispatched work
+ * @brief Typedef for a callback function used in the Bluetooth application.
  *
- * @param [in] event  event id
- * @param [in] param  handler parameter
+ * This is a function pointer type that points to a function taking a uint16_t event and a void pointer parameter,
+ * and returning void. This type is used to define the callback functions that handle various Bluetooth events in
+ * the application.
+ *
+ * @param event The event type.
+ * @param param The event parameter.
  */
-typedef void (* bt_app_cb_t) (uint16_t event, void *param);
+typedef void (*bt_app_cb_t)(uint16_t event, void *param);
+
+/**
+ * @brief Typedef for a callback function used to copy data in the Bluetooth application.
+ *
+ * This is a function pointer type that points to a function taking a destination pointer, a source pointer,
+ * and a length, and returning void. This type is used to define the callback functions that handle copying
+ * data in the application.
+ *
+ * @param p_dest Pointer to the destination where the data should be copied.
+ * @param p_src Pointer to the source from where the data should be copied.
+ * @param len The length of the data to be copied.
+ */
+typedef void (*bt_app_copy_cb_t)(void *p_dest, void *p_src, int len);
+
+/* modes of ringbuffer */
+typedef enum
+{
+    PROCESSING,  /*!< ringbuffer is buffering incoming audio data, I2S is working */
+    PREFETCHING, /*!< ringbuffer is buffering incoming audio data, I2S is waiting */
+    DROPPING     /*!< ringbuffer is not buffering (dropping) incoming audio data, I2S is working */
+} Ringbuffer_mode;
 
 /* message to be sent */
-typedef struct {
-    uint16_t       sig;      /*!< signal to bt_app_task */
-    uint16_t       event;    /*!< message event id */
-    bt_app_cb_t    cb;       /*!< context switch callback */
-    void           *param;   /*!< parameter area needs to be last */
+typedef struct
+{
+    uint16_t sig;   /*!< signal to bt_app_task */
+    uint16_t event; /*!< message event id */
+    bt_app_cb_t cb; /*!< context switch callback */
+    void *param;    /*!< parameter area needs to be last */
 } bt_app_msg_t;
 
 /**
- * @brief  parameter deep-copy function to be customized
+ * @brief Sends a message to the Bluetooth application task queue.
  *
- * @param [out] p_dest  pointer to destination data
- * @param [in]  p_src   pointer to source data
- * @param [in]  len     data length in byte
+ * This function sends a message to the task queue of the Bluetooth application.
+ * If the message is NULL or the queue is full, it logs an error and returns false.
+ *
+ * @param msg Pointer to the message to be sent. This should point to a variable of type bt_app_msg_t.
+ * @return Returns true if the message was successfully sent to the queue, false otherwise.
  */
-typedef void (* bt_app_copy_cb_t) (void *p_dest, void *p_src, int len);
+bool bt_app_send_msg(bt_app_msg_t *msg);
 
 /**
- * @brief  work dispatcher for the application task
+ * @brief Dispatches a message to its associated callback function.
  *
- * @param [in] p_cback       callback function
- * @param [in] event         event id
- * @param [in] p_params      callback paramters
- * @param [in] param_len     parameter length in byte
- * @param [in] p_copy_cback  parameter deep-copy function
+ * This function takes a message and, if the message has an associated callback function,
+ * it calls that function with the event and parameter stored in the message.
  *
- * @return  true if work dispatch successfully, false otherwise
+ * @param msg Pointer to the message to be dispatched. This should point to a variable of type bt_app_msg_t.
+ */
+void bt_app_work_dispatched(bt_app_msg_t *msg);
+
+/**
+ * @brief Handles the Bluetooth application task.
+ *
+ * This function runs an infinite loop that continuously receives messages from the task queue
+ * and dispatches them to their associated callback functions. If a message has an associated
+ * parameter, it is freed after the message is handled.
+ *
+ * @param arg Pointer to the argument for the task. This is not used in the function and can be NULL.
+ */
+void bt_app_task_handler(void *arg);
+
+/**
+ * @brief Handles the I2S task.
+ *
+ * This function runs an infinite loop that continuously checks if a semaphore is available.
+ * If the semaphore is available, it receives data from a ring buffer and writes it to the I2S DMA transmit buffer.
+ * If the ring buffer underflows, it changes the ring buffer mode to PREFETCHING and breaks the loop.
+ * After writing the data, it returns the item back to the ring buffer.
+ *
+ * @param arg Pointer to the argument for the task. This is not used in the function and can be NULL.
+ */
+void bt_i2s_task_handler(void *arg);
+
+/**
+ * @brief Dispatches a work event to the Bluetooth application task.
+ *
+ * This function creates a message with the given event and callback function, and optionally a parameter.
+ * If a parameter is provided and a copy callback function is also provided, the copy callback function
+ * is used to copy the parameter data. The message is then sent to the Bluetooth application task queue.
+ *
+ * @param p_cback The callback function to be associated with the event.
+ * @param event The event to be dispatched.
+ * @param p_params Pointer to the parameter data to be associated with the event. This can be NULL if no parameter data is needed.
+ * @param param_len The length of the parameter data in bytes. This should be 0 if no parameter data is provided.
+ * @param p_copy_cback The copy callback function to be used to copy the parameter data. This can be NULL if no deep copy is needed.
+ * @return Returns true if the message was successfully sent to the queue, false otherwise.
  */
 bool bt_app_work_dispatch(bt_app_cb_t p_cback, uint16_t event, void *p_params, int param_len, bt_app_copy_cb_t p_copy_cback);
 
 /**
- * @brief  start up the application task
+ * @brief Starts up the Bluetooth application task.
+ *
+ * This function creates a queue for Bluetooth application messages and starts the Bluetooth application task.
+ * The task runs the bt_app_task_handler function in a new task named "BtAppTask".
  */
 void bt_app_task_start_up(void);
 
 /**
- * @brief  shut down the application task
+ * @brief Shuts down the Bluetooth application task.
+ *
+ * This function deletes the Bluetooth application task and its associated queue if they exist.
+ * After deletion, the task handle and queue handle are set to NULL.
  */
 void bt_app_task_shut_down(void);
 
 /**
- * @brief  start up the is task
+ * @brief Starts up the I2S task.
+ *
+ * This function sets the ring buffer mode to PREFETCHING, creates a binary semaphore for I2S writing,
+ * creates a ring buffer for I2S data, and starts the I2S task. The task runs the bt_i2s_task_handler
+ * function in a new task named "BtI2STask". If the semaphore or ring buffer creation fails, it logs an error and returns.
  */
 void bt_i2s_task_start_up(void);
 
 /**
- * @brief  shut down the I2S task
+ * @brief Shuts down the task.
+ *
+ * This function deletes the I2S task, its associated ring buffer, and semaphore if they exist.
+ * After deletion, the task handle, ring buffer, and semaphore are set to NULL.
  */
 void bt_i2s_task_shut_down(void);
 
 /**
- * @brief  write data to ringbuffer
+ * @brief Writes data to the ring buffer.
  *
- * @param [in] data  pointer to data stream
- * @param [in] size  data length in byte
+ * This function writes a given amount of data to the ring buffer. If the ring buffer is in DROPPING mode,
+ * it drops the packet and checks if the ring buffer data has decreased enough to switch back to PROCESSING mode.
+ * If the ring buffer is in PREFETCHING mode, it checks if the ring buffer data has increased enough to switch to PROCESSING mode.
+ * If the ring buffer overflows, it switches to DROPPING mode.
  *
- * @return size if writteen ringbuffer successfully, 0 others
+ * @param data Pointer to the data to be written to the ring buffer.
+ * @param size The size of the data in bytes.
+ * @return Returns the size of the data written if the write was successful, 0 otherwise.
  */
 size_t write_ringbuf(const uint8_t *data, size_t size);
+
+/**
+ * @brief Callback function for GAP events.
+ *
+ * This function is called when a GAP event occurs. It handles various events such as authentication completion,
+ * GAP mode changes, ACL connection completion, and ACL disconnection completion. For each event, it logs the
+ * appropriate information. If an invalid event is received, it logs the event number.
+ *
+ * @param event The GAP event type.
+ * @param param The event parameter, which is cast to the appropriate type based on the event type.
+ */
+void bt_app_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param);
+
+/**
+ * @brief Handles the Bluetooth stack events.
+ *
+ * This function is called when a Bluetooth stack event occurs. It handles various events such as stack initialization.
+ * For the stack initialization event, it sets the device name, registers callbacks for GAP, AVRCP Controller, AVRCP Target,
+ * and A2DP, initializes AVRCP Controller and Target, sets the event capabilities for AVRCP Target, initializes A2DP sink,
+ * gets the default delay value for A2DP sink, and sets the device to be discoverable and connectable. If an invalid event
+ * is received, it logs an error message.
+ *
+ * @param event The Bluetooth stack event type.
+ * @param p_param The event parameter, which is not used in this function.
+ */
+void bt_av_hdl_stack_evt(uint16_t event, void *p_param);
 
 #endif /* __BT_APP_CORE_H__ */
