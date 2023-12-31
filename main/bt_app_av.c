620226e9da0e0e34a358270b0c2c2f1fc672401a
Jakub Praszkowski <kubapraszkowski@gmail.com>
2023-12-31 13:28:45 +0100

    works 4

--- main/bt_app_av.c
+++ main/bt_app_av.c
@@ -13,8 +13,6 @@
 
 /* allocate new meta buffer */
 static void bt_app_alloc_meta_buffer(esp_avrc_ct_cb_param_t *param);
-/* handler for track playing position change */
-static void bt_av_play_pos_changed(void);
 /* notification event handler */
 static void bt_av_notify_evt_handler(uint8_t event_id, esp_avrc_rn_param_t *event_parameter);
 /* installation for i2s */
@@ -67,26 +65,10 @@ static void bt_app_alloc_meta_buffer(esp_avrc_ct_cb_param_t *param)
     rc->meta_rsp.attr_text = attr_text;
 }
 
-static void bt_av_play_pos_changed(void)
-{
-    /* register notification if peer support the event_id */
-    if (esp_avrc_rn_evt_bit_mask_operation(ESP_AVRC_BIT_MASK_OP_TEST, &s_avrc_peer_rn_cap,
-                                           ESP_AVRC_RN_PLAY_POS_CHANGED))
-    {
-        esp_avrc_ct_send_register_notification_cmd(APP_RC_CT_TL_RN_PLAY_POS_CHANGE,
-                                                   ESP_AVRC_RN_PLAY_POS_CHANGED, 10);
-    }
-}
-
 static void bt_av_notify_evt_handler(uint8_t event_id, esp_avrc_rn_param_t *event_parameter)
 {
     switch (event_id)
     {
-    case ESP_AVRC_RN_PLAY_POS_CHANGED:
-        ESP_LOGI(BT_AV_TAG, "Play position changed: %" PRIu32 "-ms", event_parameter->play_pos);
-        bt_av_play_pos_changed();
-        break;
-    /* others */
     default:
         ESP_LOGI(BT_AV_TAG, "unhandled event: %d", event_id);
         break;
@@ -373,7 +355,6 @@ static void bt_av_hdl_avrc_ct_evt(uint16_t event, void *p_param)
         ESP_LOGI(BT_RC_CT_TAG, "remote rn_cap: count %d, bitmask 0x%x", rc->get_rn_caps_rsp.cap_count,
                  rc->get_rn_caps_rsp.evt_set.bits);
         s_avrc_peer_rn_cap.bits = rc->get_rn_caps_rsp.evt_set.bits;
-        bt_av_play_pos_changed();
         break;
     }
     /* others */
